7296bf291b13af466b0f388f465f12df5ee34bb6
Carmelo Atienza <atienzabusiness@gmail.com>
2024-06-19 13:48:51 +0800

    feat(login): update UI

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(private router: Router){
+
+  }
   email : string = ''
   password : string = ''
   error : string = ''
@@ -18,17 +22,16 @@ export class LoginComponent {
     this.attempts += 1
     if (this.attempts == 3){
       this.invalid = true;
+      this.error = 'You have been locked out for 1 minute';
       setTimeout(() => {
         this.invalid = false;
       }, 60000);
-    }
-    if (!this.emailPattern.test(this.email)) {
-      this.error = 'Invalid email or password! You have ' + (3 - this.attempts) + ' attempts left';
-    } else {
+    } else if (this.emailPattern.test(this.email) && this.password == 'Testpassw0rd1') {
       this.error = '';
-      // Proceed with form submission or further processing
-      console.log('Email:', this.email);
-      console.log('Password:', this.password);
+      this.router.navigate(['/home']);
+    } else {
+      this.error = 'Invalid email or password! You have ' + (3 - this.attempts) + ' attempts left';
     }
+      
   }
 }
