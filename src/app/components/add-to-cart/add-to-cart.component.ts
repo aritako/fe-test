62822b32950ce32acccbbdb668384277dce5cf18
Carmelo Atienza <atienzabusiness@gmail.com>
2024-06-19 15:35:32 +0800

    feat: integrate localStorage for addToCart

--- src/app/components/add-to-cart/add-to-cart.component.ts
+++ src/app/components/add-to-cart/add-to-cart.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-add-to-cart',
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './add-to-cart.component.css'
 })
 export class AddToCartComponent {
+  @Input() id!: any;
+  @Input() product!: any;
+  ngOnInit(){
+    console.log("THIS PRODUCT =>", this.id, this.product);
+  }
+  quantity = 1;
+  addToCart() {
+    const cartItems: { product_id: string, quantity: number }[] = JSON.parse(localStorage.getItem('cartItems') || '[]');
+    const existingItem = cartItems.find(cartItem => cartItem.product_id === this.id);
+    if (existingItem) {
+      existingItem.quantity += 1;
+    } else {
+      cartItems.push({ product_id: this.id, quantity: 1 });
+    }
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
 
+    alert('Added to cart: ' + this.id);
+  }
 }
