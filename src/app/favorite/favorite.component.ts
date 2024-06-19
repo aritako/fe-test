29d09d7fa02031534d7406260fcf59d34d4131e2
inunice <ammanguan@up.edu.ph>
2024-06-19 16:57:55 +0800

    feat: implement favorites

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import products from '../products';
 @Component({
   selector: 'app-favorite',
   standalone: true,
   imports: [],
   templateUrl: './favorite.component.html',
-  styleUrl: './favorite.component.css'
+  styleUrl: './favorite.component.css',
 })
 export class FavoriteComponent {
+  @Input() products: any[] = products;
+  favoriteItemsString = localStorage.getItem('favoriteItems');
+  favoriteItemIds: any = [];
 
+  ngOnInit() {
+    for (const favoriteItem of JSON.parse(this.favoriteItemsString || '[]')) {
+      this.favoriteItemIds.push(favoriteItem.product_id);
+      console.log(this.favoriteItemIds);
+    }
+    // for (const product in products) {
+    //   // if (this.favoriteItemIds.includes(product.product_id)) {
+    //   console.log(product.product_id);
+    //   // }
+    // }
+  }
 }
